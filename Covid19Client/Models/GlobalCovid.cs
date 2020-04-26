28ac2de60dd20624a2c8e1014b26789b9ccb272d
AlexHorton1995 <alex.d.horton95@gmail.com>
2020-04-26 11:50:30 -0500

    Created DataModel classes.

---
+++ Covid19Client/Models/GlobalCovid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Client.Models
+{
+    /// <summary>
+    /// GlobalCovid Class - contains all aggregated global data
+    /// </summary>
+    class GlobalCovid
+    {
+        public int NewConfirmed { get; set; }
+        public int TotalConfirmed { get; set; }
+        public int NewDeaths { get; set; }
+        public int TotalDeaths { get; set; }
+        public int NewRecovered { get; set; }
+        public int TotalRecovered { get; set; }
+        public List<CountryData> Countries { get; set; }
+    }
+}
--- Covid19Client/Program.cs
+++ Covid19Client/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Covid19Client.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Covid19Client
 {
+    /// <summary>
+    /// Covid19Client - A Client to consume data from the https://covid19api.com/ API.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            GlobalCovid gc = new GlobalCovid();
         }
     }
+
+
+
+
+
+
+
 }
